11bec2ba7db3c6f7840fa41b57ac8b9f12f39bd2
rioopr-beep <riokmxl@gmail.com>
2026-02-06 04:04:05 +0700

    bot.py

--- bot.py
+++ bot.py
@@ -29,14 +29,3 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
             await update.message.reply_video(video=video_file, caption="Ini videonya, Gemita! ✨")
         if os.path.exists(file_path): os.remove(file_path)
         await status.delete()
-    except Exception as e:
-        print(f"Error: {e}")
-        await status.edit_text("Gagal mengunduh. Link mungkin tidak didukung atau server sibuk.")
-
-if __name__ == '__main__':
-    print("--- MEMULAI BOT DI SERVER CLOUD ---")
-    request_config = HTTPXRequest(connect_timeout=60, read_timeout=60)
-    app = ApplicationBuilder().token(TOKEN).request(request_config).build()
-    app.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), handle_message))
-    print("--- BOT GEMITA SUDAH AKTIF ---")
-    app.run_polling()
